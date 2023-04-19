81c33be7012c486e0812dc1d0908a7b95ef9d013
Charles Bridgers IV <charlesbridgersiv@gmail.com>
2023-04-18 20:57:24 -0500

    finished chapter 4 and 5

--- request_handler.py
+++ request_handler.py
@@ -1,6 +1,6 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
-from views import get_single_animal, get_all_animals, get_single_location, get_all_locations, get_single_employee, get_all_employees, get_single_customer, get_all_customers
+from views import get_single_animal, get_all_animals, create_animal, delete_animal, get_single_location, get_all_locations, create_location, delete_location, get_single_employee, get_all_employees, create_employee, delete_employee, get_single_customer, get_all_customers, create_customer, delete_customer
 
 # Here's a class. It inherits from another class.
 # For now, think of a class as a container for functions that
@@ -13,7 +13,7 @@ class HandleRequests(BaseHTTPRequestHandler):
     """
 
     def parse_url(self, path):
-        """this is pars
+        """this is parse
         """
         # Just like splitting a string in JavaScript. If the
         # path is "/animals/1", the resulting list will
@@ -103,10 +103,8 @@ class HandleRequests(BaseHTTPRequestHandler):
         if resource == "customers":
             if id is not None:
                 response = get_single_customer(id)
-
             else:
                 response = get_all_customers()
-
         self.wfile.write(json.dumps(response).encode())
         
         self._set_headers(200)
@@ -129,13 +127,64 @@ class HandleRequests(BaseHTTPRequestHandler):
     def do_POST(self):
         """Handles POST requests to the server
         """
-        # Set response code to 'Created'
+        
         self._set_headers(201)
-
         content_len = int(self.headers.get('content-length', 0))
         post_body = self.rfile.read(content_len)
-        response = f"received post request:<br>{post_body}"
-        self.wfile.write(response.encode())
+
+        # Convert JSON string to a Python dictionary
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Initialize new animal
+        new_animal = None
+
+        # Add a new animal to the list. Don't worry about
+        # the orange squiggle, you'll define the create_animal
+        # function next.
+        if resource == "animals":
+            new_animal = create_animal(post_body)
+
+        # Encode the new animal and send in response
+            self.wfile.write(json.dumps(new_animal).encode())
+        
+        # Initialize new location
+        new_location = None
+
+        # Add a new location to the list. Don't worry about
+        # the orange squiggle, you'll define the create_location
+        # function next.
+        if resource == "locations":
+            new_location = create_location(post_body)
+
+        # Encode the new animal and send in response
+            self.wfile.write(json.dumps(new_location).encode())
+            
+        # Initialize new employee
+        new_employee = None
+
+        # Add a new employee to the list. Don't worry about
+        # the orange squiggle, you'll define the create_employee
+        # function next.
+        if resource == "employees":
+            new_employee = create_employee(post_body)
+
+        # Encode the new animal and send in response
+            self.wfile.write(json.dumps(new_employee).encode())
+            
+        # Initialize new customer
+        new_customer = None
+
+        # Add a new customer to the list. Don't worry about
+        # the orange squiggle, you'll define the create_customer
+        # function next.
+        if resource == "customers":
+            new_customer = create_customer(post_body)
+
+        # Encode the new animal and send in response
+            self.wfile.write(json.dumps(new_customer).encode())
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any PUT request.
@@ -144,6 +193,43 @@ class HandleRequests(BaseHTTPRequestHandler):
         """Handles PUT requests to the server
         """
         self.do_POST()
+        
+    def do_DELETE(self):
+        """vapor transmissions
+        """
+        # Set a 204 response code
+        self._set_headers(204)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete a single animal from the list
+        if resource == "animals":
+            delete_animal(id)
+
+            # Encode the new animal and send in response
+            self.wfile.write("".encode())
+            
+        # Delete a single customer from the list
+        if resource == "customers":
+            delete_customer(id)
+
+            # Encode the new customer and send in response
+            self.wfile.write("".encode())
+            
+        # Delete a single employee from the list
+        if resource == "employees":
+            delete_employee(id)
+
+            # Encode the new employee and send in response
+            self.wfile.write("".encode())
+            
+        # Delete a single location from the list
+        if resource == "locations":
+            delete_location(id)
+
+            # Encode the new employee and send in response
+            self.wfile.write("".encode())
 
 
 # This function is not inside the class. It is the starting
--- views/customer_requests.py
+++ views/customer_requests.py
@@ -26,3 +26,38 @@ def get_all_customers():
     """Docstring
     """
     return CUSTOMERS
+  
+def create_customer(customer):
+    """Just a Graham of Dro
+    """
+    # Get the id value of the last animal in the list
+    max_id = CUSTOMERS[-1]["id"]
+
+    # Add 1 to whatever that number is
+    new_id = max_id + 1
+
+    # Add an `id` property to the animal dictionary
+    customer["id"] = new_id
+
+    # Add the animal dictionary to the list
+    CUSTOMERS.append(customer)
+
+    # Return the dictionary with `id` property added
+    return customer
+
+def delete_customer(id):
+    """The Mighty Avengers
+    """
+    # Initial -1 value for customer index, in case one isn't found
+    customer_index = -1
+
+    # Iterate the ANIMALS list, but use enumerate() so that you
+    # can access the index value of each item
+    for index, customer in enumerate(CUSTOMERS):
+        if customer["id"] == id:
+            # Found the animal. Store the current index.
+            customer_index = index
+
+    # If the animal was found, use pop(int) to remove it from list
+    if customer_index >= 0:
+        CUSTOMERS.pop(customer_index)

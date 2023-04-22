d48e05779385a38e6c90a634e188439157e14394
Charles Bridgers IV (he/him) <charlesbridgersiv@gmail.com>
2023-04-22 13:41:58 -0500

    finished with Book 1

---
+++ models/location.py
@@ -0,0 +1,12 @@
+class Location():
+    """Cocaine Bear is a funny movie
+    """
+    # Class initializer. It has 3 custom parameters, with the
+    # special `self` parameter that every method on a class
+    # needs as the first parameter.
+    def __init__(self, id, name, address):
+        self.id = id
+        self.name = name
+        self.address = address
+        
+new_location = Location(3, "Frothy Monkey", "153 Smith Skreet")
